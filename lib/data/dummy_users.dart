1d15c7955eb3dafca6bf49f7376d9faf771b56cf
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-05-11 07:32:57 -0300

    Criação do método de criar e atualizar usuário.

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -3,30 +3,30 @@ import 'package:flutter_crud/models/user.dart';
 const DUMMY_USERS = {
   '1': User(
     id: '1',
-    name: 'Maria',
-    email: 'maria@alunos.com.br',
+    name: 'Veihmeyer',
+    email: 'veihmerzinho@umemail.com',
     avatarUrl:
-    'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png',
+    'https://cdn.pixabay.com/photo/2022/01/25/08/26/animal-6965596_960_720.jpg',
   ),
   '2': User(
     id: '2',
-    name: 'Rafael',
-    email: 'rafael@alunos.com.br',
+    name: 'Paluma',
+    email: 'rodrigopaluma@umemail.com',
     avatarUrl:
-    'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
+    'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg',
   ),
   '3': User(
     id: '3',
-    name: 'Fernanda',
-    email: 'fernanda@zymail.com.br',
+    name: 'Fernanda da Cera',
+    email: 'fernanda@umemail.com',
     avatarUrl:
-    'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png',
+    'https://cdn.pixabay.com/photo/2022/04/15/06/32/honey-bee-7133710_960_720.jpg',
   ),
   '4': User(
     id: '4',
-    name: 'Guilherme',
-    email: 'guigui@abcmail.com',
+    name: 'Gustavo Peperoni',
+    email: 'gugu_dopeperoni@umemail.com',
     avatarUrl:
-    'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+    'https://cdn.pixabay.com/photo/2021/11/12/09/14/wildebeest-6788446_960_720.jpg',
   )
 };
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/user_tile.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final Users users = Provider.of((context));
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Usuários')
-      )
+        title: Text('Lista de Usuários'),
+        actions: [
+          IconButton(onPressed: (){
+            users.put(const User(
+                id: '',
+                name: 'Ricardo Nobrega',
+                email: 'ricardo@email.com',
+                avatarUrl: '',
+            ));
+          }, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: users.count,
+          itemBuilder: (ctx, i)=> UserTile(users.byIndex(i)),
+      ),
     );
   }
 }
